661b706a96b491d1499c9952ba73a545340611fd
Stevi28 <stav.kourou19@gmail.com>
2026-03-08 19:34:18 +0200

    add_nsgs_vars_and_assocs

--- main.tf
+++ main.tf
@@ -37,8 +37,80 @@ resource "azurerm_subnet" "vm_subnet" {
 }
 
 resource "azurerm_subnet" "bastion_subnet" {
-    name = "AzureBastionSubnet"
+    name = "AzureBastionSubnet" # mandatory name
     resource_group_name = azurerm_resource_group.rg.name
     virtual_network_name = azurerm_virtual_network.vnet.name
     address_prefixes = ["10.1.2.0/26"]
 }
+
+# 3. NSGs at subnets (Recommended)
+# vms 
+resource "azurerm_network_security_group" "vm_nsg" {
+    name = "nsg-vm-subnet"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    dynamic "security_rule" {
+        for_each = var.vm_nsg_rules
+        content{
+            name = security_rule.value.name
+            priority = security_rule.value.priority
+            direction = "Inbound"
+            access = "Allow"
+            protocol = "Tcp"
+            source_port_range = "*"
+            destination_port_range = security_rule.value.port
+            source_address_prefix = security_rule.value.source # only from bastion subnet
+            destination_address_prefix = security_rule.value.destination
+        }
+    }
+}
+
+# bastion 
+resource "azurerm_network_security_group" "bastion_nsg" {
+    name = "nsg-bastion-subnet"
+    location = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+
+    dynamic "security_rule" {
+        for_each = var.bastion_inbound_nsg_rules
+        content{
+            name = security_rule.value.name
+            priority = security_rule.value.priority
+            direction = "Inbound"
+            access = "Allow"
+            protocol = "Tcp"
+            source_port_range = "*"
+            destination_port_range = security_rule.value.port
+            source_address_prefix = security_rule.value.source 
+            destination_address_prefix = "*"
+        }
+    }
+
+    dynamic "security_rule" {
+        for_each = var.bastion_outbound_nsg_rules
+        content{
+            name = security_rule.value.name
+            priority = security_rule.value.priority
+            direction = "Outbound"
+            access = "Allow"
+            protocol = "Tcp"
+            source_port_range = "*"
+            destination_port_range = security_rule.value.port
+            source_address_prefix = "*"
+            destination_address_prefix = security_rule.value.destination
+        }
+    }   
+}
+
+# association with subnets
+resource "azurerm_subnet_network_security_group_association" "vm_assoc" {
+    subnet_id = azurerm_subnet.vm_subnet.id
+    network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "bastion_assoc" {
+    subnet_id = azurerm_subnet.bastion_subnet.id
+    network_security_group_id = azurerm_network_security_group.bastion_nsg.id
+}
+
---
+++ variables.tf
@@ -0,0 +1,40 @@
+variable "vm_nsg_rules" {
+    type = list(object({
+        name = string
+        priority = number
+        port = list(string)
+        source = string
+        destination = string
+    }))
+    default = [
+        { name = "AllowBastiontoVM", priority = 100, port = ["3389", "22"], source = "10.1.2.0/26", destination = "10.1.1.0/24" }
+    ]
+}
+
+variable "bastion_inbound_nsg_rules" {
+    type = list(object({
+        name = string
+        priority = number
+        port = string
+        source = string
+    }))
+    default = [
+        { name = "AllowtoInternet", priority = 100, port = "443", source = "Internet" },
+        { name = "AllowtoGatewayManager", priority = 110, port = "443", source = "GatewayManager" },
+        { name = "AllowtoLoadBalancer", priority = 120, port = "443", source = "AzureLoadBalancer" }
+    ]
+}
+
+variable "bastion_outbound_nsg_rules" {
+    type = list(object({
+        name = string
+        priority = number
+        port = list(string)
+        destination = string
+    }))
+    default = [
+        { name = "AllowRDPSSH", priority = 130, port = ["3389", "22"], destination = "VirtualNetwork" },
+        { name = "AllowAzureCloud", priority = 140, port = "443", destination = "AzureCloud" },
+        { name = "AllowSessionInfo", priority = 150, port = "80", destination = "Internet" }
+    ]
+}
